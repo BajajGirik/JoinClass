00356b9fe83994ccbd9e899ae5fab4df8915ecbd
Girik Bajaj <bajajgirik2010@gmail.com>
2021-05-26 12:51:44 +0530

    Pressing tab to focus on console and using time module to let the page load

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import webbrowser
 import pyautogui
+import time
 
 #Taking the meeting link from the user
 url = input("Enter the meeting link: ")
 browser = webbrowser.open(url)
+time.sleep(2)
+
+#Opening console on browser
+pyautogui.press('f12')
+time.sleep(1)
+
+#Pressing tab to focus the curson on console
+pyautogui.press('tab')
