7e927400a11a0b738c864dbc02aa85bd6d400983
Girik Bajaj <bajajgirik2010@gmail.com>
2021-05-26 21:47:14 +0530

    Fixed Bug: Keyboard presses to fast (Keys not registering)

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 
 #Taking the meeting link from the user
 url = input("Enter the meeting link: ")
-url += '?authuser=1'
+
 # Use the below line to enter using a different account 
 # url += f'?authuser={input("Select the account number with which you want to login: ")}'
 
@@ -20,19 +20,19 @@ pyautogui.press('tab')
 
 #To write javascript on console
 #Storing the mic button and video button in variable x
-pyautogui.write('let x=document.querySelectorAll(".U26fgb")')
+pyautogui.write('let x=document.querySelectorAll(".U26fgb")',interval = 0.015)
 pyautogui.press('enter')
 
 #Clicking the mic button
-pyautogui.write('x[0].click()')
+pyautogui.write('x[0].click()',interval = 0.015)
 pyautogui.press('enter')
 
 #Clicking the video button
-pyautogui.write('x[1].click()')
+pyautogui.write('x[1].click()',interval = 0.015)
 pyautogui.press('enter')
 
 #Clicking the join now button
-pyautogui.write('document.querySelector(".uArJ5e").click()')
+pyautogui.write('document.querySelector(".uArJ5e").click()',interval = 0.02)
 pyautogui.press('enter')
 
 #To remove console from screen
